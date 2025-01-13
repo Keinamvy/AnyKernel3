9c646d79bf488cea51e3fdeb9e0d1f5ea63d2c76
Keinamvy <92191018+Keinamvy@users.noreply.github.com>
2025-01-13 22:35:54 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 12 Pro 4G by t.me/itsshashanksp
+kernel.string=Vantom Kernel for Redmi Note 12 Pro 4G
 do.devicecheck=1
 do.modules=0
 do.systemless=1
